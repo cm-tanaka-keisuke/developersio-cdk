540c304d32247ca79810ddb6bddc37217890cacf
tanaka.keisuke <tanaka.keisuke@classmethod.jp>
2021-05-15 19:23:19 +0900

    VPC を実装

--- lib/devio-stack.ts
+++ lib/devio-stack.ts
@@ -1,9 +1,10 @@
 import * as cdk from '@aws-cdk/core';
+import { Vpc } from '@aws-cdk/aws-ec2';
 
 export class DevioStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new Vpc(this, 'Vpc');
   }
 }
