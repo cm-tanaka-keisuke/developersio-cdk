370648498528fab813eaa3cf878e7dbc2500abf6
tanaka.keisuke <tanaka.keisuke@classmethod.jp>
2021-05-18 14:06:10 +0900

    VPC のテストコードを実装

--- test/devio.test.ts
+++ test/devio.test.ts
@@ -1,13 +1,14 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import { expect, countResources, haveResource } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as Devio from '../lib/devio-stack';
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new Devio.DevioStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+test('Vpc', () => {
+  const app = new cdk.App();
+  const stack = new Devio.DevioStack(app, 'Vpc');
+
+  expect(stack).to(countResources('AWS::EC2::VPC', 1));
+  expect(stack).to(haveResource('AWS::EC2::VPC', {
+    CidrBlock: '10.0.0.0/16',
+    Tags: [{ 'Key': 'Name', 'Value': 'devio-stg-vpc' }]
+  }));
 });
