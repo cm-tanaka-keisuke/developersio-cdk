a98a121dd4629f911728982c53e73271cb6e28e4
tanaka.keisuke <tanaka.keisuke@classmethod.jp>
2021-05-20 16:49:32 +0900

    Context を設定
    - systemName
    - envType

--- lib/devio-stack.ts
+++ lib/devio-stack.ts
@@ -5,9 +5,12 @@ export class DevioStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const systemName = this.node.tryGetContext('systemName');
+    const envType = this.node.tryGetContext('envType');
+
     new CfnVPC(this, 'Vpc', {
       cidrBlock: '10.0.0.0/16',
-      tags: [{ key: 'Name', value: 'devio-stg-vpc' }]
+      tags: [{ key: 'Name', value: `${systemName}-${envType}-vpc` }]
     });
   }
 }
