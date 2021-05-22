b515caf384281300fca39ada13080001e2d404d8
tanaka.keisuke <tanaka.keisuke@classmethod.jp>
2021-05-22 12:41:03 +0900

    サブネットのテストコードを実装

--- test/devio.test.ts
+++ test/devio.test.ts
@@ -9,7 +9,7 @@ test('Context', () => {
       'envType': 'prd'
     }
   });
-  const stack = new Devio.DevioStack(app, 'Context');
+  const stack = new Devio.DevioStack(app, 'DevioStack');
 
   expect(stack).to(haveResource('AWS::EC2::VPC', {
     Tags: [{ 'Key': 'Name', 'Value': 'starwars-prd-vpc' }]
@@ -18,11 +18,48 @@ test('Context', () => {
 
 test('Vpc', () => {
   const app = new cdk.App();
-  const stack = new Devio.DevioStack(app, 'Vpc');
+  const stack = new Devio.DevioStack(app, 'DevioStack');
 
   expect(stack).to(countResources('AWS::EC2::VPC', 1));
   expect(stack).to(haveResource('AWS::EC2::VPC', {
     CidrBlock: '10.0.0.0/16',
     Tags: [{ 'Key': 'Name', 'Value': 'undefined-undefined-vpc' }]
+  }));
+});
+
+test('Subnet', () => {
+  const app = new cdk.App();
+  const stack = new Devio.DevioStack(app, 'DevioStack');
+
+  expect(stack).to(countResources('AWS::EC2::Subnet', 6));
+  expect(stack).to(haveResource('AWS::EC2::Subnet', {
+    CidrBlock: '10.0.11.0/24',
+    AvailabilityZone: 'ap-northeast-1a',
+    Tags: [{ 'Key': 'Name', 'Value': 'undefined-undefined-subnet-public-1a' }]
+  }));
+  expect(stack).to(haveResource('AWS::EC2::Subnet', {
+    CidrBlock: '10.0.12.0/24',
+    AvailabilityZone: 'ap-northeast-1c',
+    Tags: [{ 'Key': 'Name', 'Value': 'undefined-undefined-subnet-public-1c' }]
+  }));
+  expect(stack).to(haveResource('AWS::EC2::Subnet', {
+    CidrBlock: '10.0.21.0/24',
+    AvailabilityZone: 'ap-northeast-1a',
+    Tags: [{ 'Key': 'Name', 'Value': 'undefined-undefined-subnet-app-1a' }]
+  }));
+  expect(stack).to(haveResource('AWS::EC2::Subnet', {
+    CidrBlock: '10.0.22.0/24',
+    AvailabilityZone: 'ap-northeast-1c',
+    Tags: [{ 'Key': 'Name', 'Value': 'undefined-undefined-subnet-app-1c' }]
+  }));
+  expect(stack).to(haveResource('AWS::EC2::Subnet', {
+    CidrBlock: '10.0.31.0/24',
+    AvailabilityZone: 'ap-northeast-1a',
+    Tags: [{ 'Key': 'Name', 'Value': 'undefined-undefined-subnet-db-1a' }]
+  }));
+  expect(stack).to(haveResource('AWS::EC2::Subnet', {
+    CidrBlock: '10.0.32.0/24',
+    AvailabilityZone: 'ap-northeast-1c',
+    Tags: [{ 'Key': 'Name', 'Value': 'undefined-undefined-subnet-db-1c' }]
   }));
 });
