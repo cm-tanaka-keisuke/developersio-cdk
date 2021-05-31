606ad5ab66a75f56f7cf12badbb1e08002ccdedc
tanaka.keisuke <tanaka.keisuke@classmethod.jp>
2021-05-31 18:30:56 +0900

    NAT ゲートウェイを実装

--- lib/devio-stack.ts
+++ lib/devio-stack.ts
@@ -3,6 +3,7 @@ import { Vpc } from './resource/vpc';
 import { Subnet } from './resource/subnet';
 import { InternetGateway } from './resource/internetGateway';
 import { ElasticIp } from './resource/elasticIp';
+import { NatGateway } from './resource/natGateway';
 
 export class DevioStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -23,5 +24,14 @@ export class DevioStack extends cdk.Stack {
     // Elastic IP
     const elasticIp = new ElasticIp();
     elasticIp.createResources(this);
+
+    // NAT Gateway
+    const natGateway = new NatGateway(
+      subnet.public1a,
+      subnet.public1c,
+      elasticIp.ngw1a,
+      elasticIp.ngw1c
+    );
+    natGateway.createResources(this);
   }
 }
