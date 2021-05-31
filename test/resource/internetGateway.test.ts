8436e842ec0f13b7034f5d65523f54386b82ab7b
tanaka.keisuke <tanaka.keisuke@classmethod.jp>
2021-05-31 10:31:45 +0900

    インターネットゲートウェイのテストコードを実装

---
+++ test/resource/internetGateway.test.ts
@@ -0,0 +1,15 @@
+import { expect, countResources, haveResource } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as Devio from '../../lib/devio-stack';
+
+test('InternetGateway', () => {
+    const app = new cdk.App();
+    const stack = new Devio.DevioStack(app, 'DevioStack');
+
+    expect(stack).to(countResources('AWS::EC2::InternetGateway', 1));
+    expect(stack).to(haveResource('AWS::EC2::InternetGateway', {
+        Tags: [{ 'Key': 'Name', 'Value': 'undefined-undefined-igw' }]
+    }));
+
+    expect(stack).to(countResources('AWS::EC2::VPCGatewayAttachment', 1));
+});
