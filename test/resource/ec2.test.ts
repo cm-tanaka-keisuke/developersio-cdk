eb63e84d6e9905b5e06bbc83c3a0ae88e5be07bc
tanaka.keisuke <tanaka.keisuke@classmethod.jp>
2021-06-29 13:32:42 +0900

    EC2 のテストコードを実装

---
+++ test/resource/ec2.test.ts
@@ -0,0 +1,28 @@
+import { expect, countResources, haveResource, anything } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as Devio from '../../lib/devio-stack';
+
+test('Ec2', () => {
+    const app = new cdk.App();
+    const stack = new Devio.DevioStack(app, 'DevioStack');
+
+    expect(stack).to(countResources('AWS::EC2::Instance', 2));
+    expect(stack).to(haveResource('AWS::EC2::Instance', {
+        AvailabilityZone: 'ap-northeast-1a',
+        IamInstanceProfile: anything(),
+        ImageId: 'ami-06631ebafb3ae5d34',
+        InstanceType: 't2.micro',
+        SecurityGroupIds: anything(),
+        SubnetId: anything(),
+        Tags: [{ 'Key': 'Name', 'Value': 'undefined-undefined-ec2-1a' }]
+    }));
+    expect(stack).to(haveResource('AWS::EC2::Instance', {
+        AvailabilityZone: 'ap-northeast-1c',
+        IamInstanceProfile: anything(),
+        ImageId: 'ami-06631ebafb3ae5d34',
+        InstanceType: 't2.micro',
+        SecurityGroupIds: anything(),
+        SubnetId: anything(),
+        Tags: [{ 'Key': 'Name', 'Value': 'undefined-undefined-ec2-1c' }]
+    }));
+});
