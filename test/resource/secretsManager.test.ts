c19e38895df4198dbab462d3552532d536eac8d7
tanaka.keisuke <tanaka.keisuke@classmethod.jp>
2021-07-03 11:50:07 +0900

    Secrets Manager のテストコードを実装

---
+++ test/resource/secretsManager.test.ts
@@ -0,0 +1,20 @@
+import { expect, countResources, haveResource } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as Devio from '../../lib/devio-stack';
+
+test('SecretsManager', () => {
+    const app = new cdk.App();
+    const stack = new Devio.DevioStack(app, 'DevioStack');
+
+    expect(stack).to(countResources('AWS::SecretsManager::Secret', 1));
+    expect(stack).to(haveResource('AWS::SecretsManager::Secret', {
+        Description: 'for RDS cluster',
+        GenerateSecretString: {
+            ExcludeCharacters: '"@/\\',
+            GenerateStringKey: 'MasterUserPassword',
+            PasswordLength: 16,
+            SecretStringTemplate: '{"MasterUsername": "admin"}'
+        },
+        Name: 'undefined-undefined-secrets-rds-cluster'
+    }));
+});
