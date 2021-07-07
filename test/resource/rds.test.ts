5b47b387b410e7d67a54c603337b04c4f8d06465
tanaka.keisuke <tanaka.keisuke@classmethod.jp>
2021-07-07 14:01:29 +0900

    RDS サブネットグループのテストコードを実装

---
+++ test/resource/rds.test.ts
@@ -0,0 +1,15 @@
+import { expect, countResources, haveResource, anything } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as Devio from '../../lib/devio-stack';
+
+test('Rds', () => {
+    const app = new cdk.App();
+    const stack = new Devio.DevioStack(app, 'DevioStack');
+
+    expect(stack).to(countResources('AWS::RDS::DBSubnetGroup', 1));
+    expect(stack).to(haveResource('AWS::RDS::DBSubnetGroup', {
+        DBSubnetGroupDescription: 'Subnet Group for RDS',
+        SubnetIds: anything(),
+        DBSubnetGroupName: 'undefined-undefined-sng-rds'
+    }));
+});
