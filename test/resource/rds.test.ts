ffa389e1f84aedbaa48ccd9964954a77bbd665e6
tanaka.keisuke <tanaka.keisuke@classmethod.jp>
2021-07-09 12:56:19 +0900

    RDS クラスターのテストコードを実装

--- test/resource/rds.test.ts
+++ test/resource/rds.test.ts
@@ -25,4 +25,24 @@ test('Rds', () => {
         Description: 'Parameter Group for RDS',
         Family: 'aurora-mysql5.7'
     }));
+
+    expect(stack).to(countResources('AWS::RDS::DBCluster', 1));
+    expect(stack).to(haveResource('AWS::RDS::DBCluster', {
+        Engine: 'aurora-mysql',
+        BackupRetentionPeriod: 7,
+        DatabaseName: 'devio',
+        DBClusterIdentifier: 'undefined-undefined-rds-cluster',
+        DBClusterParameterGroupName: anything(),
+        DBSubnetGroupName: anything(),
+        EnableCloudwatchLogsExports: ['error'],
+        EngineMode: 'provisioned',
+        EngineVersion: '5.7.mysql_aurora.2.10.0',
+        MasterUsername: anything(),
+        MasterUserPassword: anything(),
+        Port: 3306,
+        PreferredBackupWindow: '19:00-19:30',
+        PreferredMaintenanceWindow: 'sun:19:30-sun:20:00',
+        StorageEncrypted: true,
+        VpcSecurityGroupIds: anything()
+    }));
 });
