1cf0229edbcbeda88d0753e27fa588a34eb714f8
tanaka.keisuke <tanaka.keisuke@classmethod.jp>
2021-12-16 12:56:35 +0900

    ネットワーク ACL のテストコードを実装

--- test/stack/vpc-stack.test.ts
+++ test/stack/vpc-stack.test.ts
@@ -127,4 +127,41 @@ test('Vpc Stack', () => {
         RouteTableId: Match.anyValue(),
         SubnetId: Match.anyValue()
     });
+
+    // Network ACL
+    template.resourceCountIs('AWS::EC2::NetworkAcl', 3);
+    template.hasResourceProperties('AWS::EC2::NetworkAcl', {
+        VpcId: Match.anyValue(),
+        Tags: [{ Key: 'Name', Value: 'devio-stg-nacl-public' }]
+    });
+    template.hasResourceProperties('AWS::EC2::NetworkAcl', {
+        VpcId: Match.anyValue(),
+        Tags: [{ Key: 'Name', Value: 'devio-stg-nacl-app' }]
+    });
+    template.hasResourceProperties('AWS::EC2::NetworkAcl', {
+        VpcId: Match.anyValue(),
+        Tags: [{ Key: 'Name', Value: 'devio-stg-nacl-db' }]
+    });
+    template.resourceCountIs('AWS::EC2::NetworkAclEntry', 6);
+    template.hasResourceProperties('AWS::EC2::NetworkAclEntry', {
+        NetworkAclId: Match.anyValue(),
+        Protocol: -1,
+        RuleAction: 'allow',
+        RuleNumber: 100,
+        CidrBlock: '0.0.0.0/0',
+        Egress: false
+    });
+    template.hasResourceProperties('AWS::EC2::NetworkAclEntry', {
+        NetworkAclId: Match.anyValue(),
+        Protocol: -1,
+        RuleAction: 'allow',
+        RuleNumber: 100,
+        CidrBlock: '0.0.0.0/0',
+        Egress: true
+    });
+    template.resourceCountIs('AWS::EC2::SubnetNetworkAclAssociation', 6);
+    template.hasResourceProperties('AWS::EC2::SubnetNetworkAclAssociation', {
+        NetworkAclId: Match.anyValue(),
+        SubnetId: Match.anyValue()
+    });
 });
