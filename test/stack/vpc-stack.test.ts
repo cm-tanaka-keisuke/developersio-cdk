a1564d85b86f4af090049e22e26fa4a73395d854
tanaka.keisuke <tanaka.keisuke@classmethod.jp>
2021-12-16 11:58:12 +0900

    サブネットのテストコードを実装

--- test/stack/vpc-stack.test.ts
+++ test/stack/vpc-stack.test.ts
@@ -1,5 +1,5 @@
 import { App } from 'aws-cdk-lib';
-import { Template } from 'aws-cdk-lib/assertions';
+import { Match, Template } from 'aws-cdk-lib/assertions';
 import { VpcStack } from '../../lib/stack/vpc-stack';
 
 test('Vpc Stack', () => {
@@ -17,5 +17,44 @@ test('Vpc Stack', () => {
     template.hasResourceProperties('AWS::EC2::VPC', {
         CidrBlock: '10.0.0.0/16',
         Tags: [{ Key: 'Name', Value: 'devio-stg-vpc' }]
+    });
+
+    // Subnet
+    template.resourceCountIs('AWS::EC2::Subnet', 6);
+    template.hasResourceProperties('AWS::EC2::Subnet', {
+        CidrBlock: '10.0.11.0/24',
+        VpcId: Match.anyValue(),
+        AvailabilityZone: 'ap-northeast-1a',
+        Tags: [{ Key: 'Name', Value: 'devio-stg-subnet-public-1a' }]
+    });
+    template.hasResourceProperties('AWS::EC2::Subnet', {
+        CidrBlock: '10.0.12.0/24',
+        VpcId: Match.anyValue(),
+        AvailabilityZone: 'ap-northeast-1c',
+        Tags: [{ Key: 'Name', Value: 'devio-stg-subnet-public-1c' }]
+    });
+    template.hasResourceProperties('AWS::EC2::Subnet', {
+        CidrBlock: '10.0.21.0/24',
+        VpcId: Match.anyValue(),
+        AvailabilityZone: 'ap-northeast-1a',
+        Tags: [{ Key: 'Name', Value: 'devio-stg-subnet-app-1a' }]
+    });
+    template.hasResourceProperties('AWS::EC2::Subnet', {
+        CidrBlock: '10.0.22.0/24',
+        VpcId: Match.anyValue(),
+        AvailabilityZone: 'ap-northeast-1c',
+        Tags: [{ Key: 'Name', Value: 'devio-stg-subnet-app-1c' }]
+    });
+    template.hasResourceProperties('AWS::EC2::Subnet', {
+        CidrBlock: '10.0.31.0/24',
+        VpcId: Match.anyValue(),
+        AvailabilityZone: 'ap-northeast-1a',
+        Tags: [{ Key: 'Name', Value: 'devio-stg-subnet-db-1a' }]
+    });
+    template.hasResourceProperties('AWS::EC2::Subnet', {
+        CidrBlock: '10.0.32.0/24',
+        VpcId: Match.anyValue(),
+        AvailabilityZone: 'ap-northeast-1c',
+        Tags: [{ Key: 'Name', Value: 'devio-stg-subnet-db-1c' }]
     });
 });
