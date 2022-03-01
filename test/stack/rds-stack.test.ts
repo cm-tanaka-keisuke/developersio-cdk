c42d116d6d9758179e54b2919abf487951bd6b9d
tanaka.keisuke <tanaka.keisuke@classmethod.jp>
2022-03-01 11:45:08 +0900

    RDS DB クラスターのテストコードを実装

--- test/stack/rds-stack.test.ts
+++ test/stack/rds-stack.test.ts
@@ -1,6 +1,9 @@
 import { App } from 'aws-cdk-lib';
 import { Match, Template } from 'aws-cdk-lib/assertions';
+import { Ec2Stack } from '../../lib/stack/ec2-stack';
+import { IamStack } from '../../lib/stack/iam-stack';
 import { RdsStack } from '../../lib/stack/rds-stack';
+import { SecretsManagerStack } from '../../lib/stack/secrets-manager-stack';
 import { VpcStack } from '../../lib/stack/vpc-stack';
 
 test('Rds Stack', () => {
@@ -11,7 +14,10 @@ test('Rds Stack', () => {
         }
     });
     const vpcStack = new VpcStack(app, 'VpcStack');
-    const rdsStack = new RdsStack(app, 'RdsStack', vpcStack);
+    const iamStack = new IamStack(app, 'IamStack');
+    const ec2Stack = new Ec2Stack(app, 'Ec2Stack', vpcStack, iamStack);
+    const secretsManagerStack = new SecretsManagerStack(app, 'SecretsManagerStack');
+    const rdsStack = new RdsStack(app, 'RdsStack', vpcStack, iamStack, ec2Stack, secretsManagerStack);
     const template = Template.fromStack(rdsStack);
 
     // Subnet Group
@@ -29,10 +35,30 @@ test('Rds Stack', () => {
         Family: 'aurora-mysql5.7',
         Parameters: { time_zone: 'UTC' }
     });
-
     template.resourceCountIs('AWS::RDS::DBParameterGroup', 1);
     template.hasResourceProperties('AWS::RDS::DBParameterGroup', {
         Description: 'Parameter Group for RDS',
         Family: 'aurora-mysql5.7'
+    });
+
+    // DB Cluster
+    template.resourceCountIs('AWS::RDS::DBCluster', 1);
+    template.hasResourceProperties('AWS::RDS::DBCluster', {
+        Engine: 'aurora-mysql',
+        BackupRetentionPeriod: 7,
+        DatabaseName: 'devio',
+        DBClusterIdentifier: 'devio-stg-rds-cluster',
+        DBClusterParameterGroupName: Match.anyValue(),
+        DBSubnetGroupName: Match.anyValue(),
+        EnableCloudwatchLogsExports: ['error'],
+        EngineMode: 'provisioned',
+        EngineVersion: '5.7.mysql_aurora.2.10.0',
+        MasterUsername: Match.anyValue(),
+        MasterUserPassword: Match.anyValue(),
+        Port: 3306,
+        PreferredBackupWindow: '19:00-19:30',
+        PreferredMaintenanceWindow: 'sun:19:30-sun:20:00',
+        StorageEncrypted: true,
+        VpcSecurityGroupIds: Match.anyValue()
     });
 });
