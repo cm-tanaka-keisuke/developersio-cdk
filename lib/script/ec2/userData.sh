219600117cdbf5591c718cf4a696e03d86801d53
tanaka.keisuke <tanaka.keisuke@classmethod.jp>
2022-03-04 09:35:26 +0900

    EC2 の UserData を更新
    - MySQL の新たな GnuPG キーをインポート

--- lib/script/ec2/userData.sh
+++ lib/script/ec2/userData.sh
@@ -9,4 +9,5 @@ sudo systemctl start httpd
 sudo yum -y install https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
 sudo yum-config-manager --disable mysql80-community
 sudo yum-config-manager --enable mysql57-community
+sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2022
 sudo yum -y install mysql-community-client
